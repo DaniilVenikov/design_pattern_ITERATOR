8c4ae8d5d3dab32923c1cffcc7b17e0d59cbc612
DaniilVenikov <daniilvenikoff@gmail.com>
2022-10-21 12:51:53 +0300

    modified Randoms

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -7,15 +7,12 @@ public class Randoms implements Iterable<Integer>{
     private final Random random;
     private final int minItem;
     private final int maxItem;
-    private final List<Integer> list = new ArrayList<>();
-    private int nextItem;
 
     public Randoms(int min, int max){
         this.minItem = min;
         this.maxItem = max;
         random = new Random();
-        list.add(minItem + random.nextInt(maxItem-minItem+1));
-        nextItem = 0;
+
     }
 
     @Override
@@ -25,17 +22,12 @@ public class Randoms implements Iterable<Integer>{
 
             @Override
             public boolean hasNext() {
-                return !list.isEmpty();
+                return true;
             }
 
             @Override
             public Integer next() {
-                if (nextItem >= list.size()){
-                    list.add(minItem + random.nextInt(maxItem-minItem+1));
-                }
-                int currentItem = list.get(nextItem);
-                nextItem++;
-                return currentItem;
+                    return minItem + random.nextInt(maxItem-minItem+1);
             }
         };
     }
